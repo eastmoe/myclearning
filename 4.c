2b16803bc7bd26697acb7f8914846a10cb154555
Skylake Moonlight <31646341+eastmoe@users.noreply.github.com>
2017-10-27 21:06:26 +0800

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-int fun(int x)
+int fun(int x)                          //定义fun函数
 {
-	int y;
+	int y;                          //定义返回变量
 	if (x > 0)
-		y = 1;
+	    y = 1;
 	else if (x = 0)
-		y = 0;
+	    y = 0;
 	else
-		y = -1;
-	return y;
+	    y = -1;
+	return y;                       //返回变量y
 }
 void main()
 {
 	int x;
-	scanf("%d", &x);
-	printf("%d\n", fun(x));
+	scanf("%d", &x);                //输入x
+	printf("%d\n", fun(x));         //调用fun函数判断正负并输出
 	system("pause");
 }
