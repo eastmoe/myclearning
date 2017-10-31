cb7457bbd7ff7bb7831361deb8af633d91597271
Skylake Moonlight <31646341+eastmoe@users.noreply.github.com>
2017-10-31 19:38:58 +0800

    Create exp2.2.c

---
+++ exp2.2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int f;
+void main()
+ {
+  scanf("%d",f); //输入分数
+  if(f=>90)
+  {
+   printf("The grade is A."); //分数大于等于90,输出A
+  }
+  else
+  {
+   if(f=>75)
+   {
+    printf("The grade is B"); //分数在[75,89]之间，输出B
+   }
+   else
+   {
+    if(f=>60)
+    {
+     printf("The grade is C"); //分数在[60,74]之间，输出C
+    }
+    else
+    {
+     printf("The grade is D"); //分数小于等于59,输出D
+    }
+   }
+  }
+ 
+ }
