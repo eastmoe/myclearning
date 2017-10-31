b38c03582fd820a83a57559a6906014b9c9727ac
Skylake Moonlight <31646341+eastmoe@users.noreply.github.com>
2017-10-31 19:47:52 +0800

    Create exp2.3.1.c

---
+++ exp2.3.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int a,b,t //定义变量a,b,t
+void main()
+{
+ scanf("a=%d b=%d",a,b); //输入a和b
+ t=a; //将a的值赋给t
+ a=b; //将b的值赋给a
+ b=t; //将t的值赋给b
+ printf("a=%d b=%d",a,b); //输出a与b的值
+}
