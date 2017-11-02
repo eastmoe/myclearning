7d157e88f4f16fc6379e87c82d9a7907100dc00c
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-11-02 08:33:15 +0800

    Update exp2.3.1.c

--- exp2.3.1.c
+++ exp2.3.1.c
@@ -2,9 +2,9 @@
 int a,b,t //定义变量a,b,t
 void main()
 {
- scanf("a=%d b=%d",a,b); //输入a和b
+ scanf("a=%d b=%d",&a,&b); //输入a和b
  t=a; //将a的值赋给t
  a=b; //将b的值赋给a
  b=t; //将t的值赋给b
- printf("a=%d b=%d",a,b); //输出a与b的值
+ printf("a=%d b=%d\n",a,b); //输出a与b的值
 }
