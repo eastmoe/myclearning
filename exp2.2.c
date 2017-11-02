8ac2de77c38e03e0e47c35623a4604dea96f6e77
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-11-02 08:38:10 +0800

    Update exp2.2.c

--- exp2.2.c
+++ exp2.2.c
@@ -5,23 +5,23 @@ void main()
   scanf("%d",&f); //输入分数
   if(f>=90)
   {
-   printf("The grade is A."); //分数大于等于90,输出A
+   printf("The grade is A.\n"); //分数大于等于90,输出A
   }
   else
   {
    if(f>=75)
    {
-    printf("The grade is B."); //分数在[75,89]之间，输出B
+    printf("The grade is B.\n"); //分数在[75,89]之间，输出B
    }
    else
    {
     if(f>=60)
     {
-     printf("The grade is C."); //分数在[60,74]之间，输出C
+     printf("The grade is C.\n"); //分数在[60,74]之间，输出C
     }
     else
     {
-     printf("The grade is D."); //分数小于等于59,输出D
+     printf("The grade is D.\n"); //分数小于等于59,输出D
     }
    }
   }
