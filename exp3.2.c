227c72ff32d4a83f5f8851bc20f8fad546394204
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-11-07 20:07:24 +0800

    Create exp3.2.c
    Compute 1*1+2*2+3*3+...+10*10.

---
+++ exp3.2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+ int i; //定义计数器为整型变量i
+ long int result; //定义结果为长整型变量result
+ result=0; //初始化result
+ for(i=1;i<=10;i++)
+ {
+  result=result+i*i; //result=上次结果+本次计数器的平方
+ }
+ printf("Result=%ld", result); //输出result
+}
