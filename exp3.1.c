fd2df4d4223180f5a78bb268dbf1715ccb73ce3d
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-11-16 07:53:46 +0800

    Update exp3.1.c

--- exp3.1.c
+++ exp3.1.c
@@ -2,7 +2,7 @@
 void main()
 {
  int n,i,t; //定义3个变量作为数n，循环计数器i和结果计数器t
- scanf("n=%d",n); //输入n
+ scanf("n=%d",&n); //输入n
  t=0;
  for(i=1;i<=n;i++)
  {
@@ -20,5 +20,5 @@ void main()
   }
 
  }
- printf("%d",t); //输出结果
+ printf("%d\n",t); //输出结果
 }
