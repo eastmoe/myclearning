c474da7c569bc4c36ee0ed704f3a3e15103a1bd8
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-11-20 06:34:34 -0800

    Update exp4.2.c

--- exp4.2.c
+++ exp4.2.c
@@ -5,11 +5,11 @@ reverse(int number)  //定义整型函数reverse
 	{
 		int a;  //定义变量a，用于存放单个数字
 	 	a=number%10;  //a被赋值为number除以10的余数
-    printf("%d",a);  //输出a
+                printf("%d",a);  //输出a
 		number=number/10;  //number被赋值为自身的十分之一
-	  reverse(number);  //递归调用reverse
+	        reverse(number);  //递归调用reverse
 	}
-  else
+        else
 	{
 		printf("\n");  //如果number为0，输出换行符表示结束
 	}
@@ -18,8 +18,8 @@ reverse(int number)  //定义整型函数reverse
 
 void main()
 {
-	static int number;//定义静态变量number
-  printf("请输入一个数Number:");
+	static int number;  //定义静态变量number
+        printf("请输入一个数number:");
 	scanf("%d",&number);  //输入number
 	reverse(number);  //调用函数reverse
 }
