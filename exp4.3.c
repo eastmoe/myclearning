5b94ec7838df6f557ed6d4d67fbcb7ed9bdce993
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-11-21 18:40:46 +0800

    Create exp4.3.c
    Repair the program which can exchange two numbers.

---
+++ exp4.3.c
@@ -0,0 +1,20 @@
+//原程序
+#include <stdio.h>
+void main()
+{
+ int temp;
+ temp=x;
+ x=y;
+ y=temp;
+
+}
+void main()
+{
+ int number1=10;
+ int number2=20;
+ printf("交换前的值：\n");
+ printf("number1==%d\tnumber2=%d\n",number1,number2);
+ swap(number1,number2);
+ printf("交换前的值：\n");
+ printf("number1==%d\tnumber2=%d\n",number1,number2);
+}
