98be30dd5e18e778f5f9cc6c5657c1eab90b9ee8
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-11-22 06:16:52 -0800

    Create exp5.1.c
    Define a two-dimensional array array[3][4] and assign it with the keyboard, and find the maximum and minimum values in the array.

---
+++ exp5.1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main()
+{
+int i,j,min,max,array[3][4];  \\定义变量及数组
+
+for (i=0;i<3;i++)  \\行循环
+{
+	for (j=0;j<4;j++)  \\列循环
+	{
+		printf("请输入数组Array(%d，%d)的值",i,j);
+		scanf("%d",&array[i][j]);
+	}
+}
+min=array[0][0];  \\初始化最大值和最小值变量为数组中第一个元素
+max=array[0][0];
+for (i=0;i<3;i++)  \\行循环
+{
+	for (j=0;j<4;j++)  \\列循环
+	{
+		if(array[i][j]>max)  \\判断当前元素是否大于max
+		{
+			max=array[i][j];  \\是则将当前元素的值赋给max
+		}
+		if(array[i][j]<min)  \\判断当前元素是否小于min
+		{
+			min=array[i][j];  \\是则将当前元素的值赋给min
+		}
+	}
+}
+printf("数组Array中最大的值为%d\n",max);  \\输出最大值和最小值
+printf("数组Array中最小的值为%d\n",min);
+}
