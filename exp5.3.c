a8d0a2953cff3b02329215062f2fe07bc2682eb1
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-11-29 20:41:13 +0800

    Update exp5.3.c

--- exp5.3.c
+++ exp5.3.c
@@ -1,4 +1,37 @@
+//—-----------------------常规方法--------------------------------------------
 #include<stdio.h>
+void main()
+{
+ int array[10],i,j,temp;  //定义数组array，循环变量i，j以及指针p
+ for(i=0;i<=9;i++)  //输入循环
+ {
+  printf("请输入数组Array中的元素Array[%d]的值：",i+1);
+  scanf("%d",&array[i]);
+ }
+ for(i=0;i<10;i++)
+ {
+  for(j=0;j<9;j++)
+  {
+   if(array[j]>array[j+1])  //判断元素array[j]是否大于array[j+1]
+   {
+    temp=array[j];
+    array[j]=array[j+1];  //交换变量
+	array[j+1]=temp;
+   }
+  }
+ }
+
+ printf("数组Array中数从小到大排列为：\n");
+ for(i=0;i<=9;i++)
+ {
+  printf("%d ",array[i]);
+ }
+ printf("\n");
+}
+//--------------------常规方法：结束------------------------------------------------------
+//-----------------------利用指针------------------------------------------------------
+#include<stdio.h>
+
 void intexchanger(int *x,int *y)  //定义函数intexchanger，用指针交换变量的值
 {
  int temp;
@@ -27,10 +60,11 @@ void main()
   }
  }
  p=array;  //初始化指针p，准备输出数据
- printf("数组Array中数从小到大排列为：\n");
+ printf ("数组Array中数从小到大排列为：\n");
  for(i=0;i<=9;i++)
  {
-  printf("%d ",p+i);
+  printf("%d ",*(p+i));
  }
  printf("\n");
 }
+//---------------------------利用指针:结束----------------------------------------
