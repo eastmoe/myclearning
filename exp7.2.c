acae33859ad09d023e93431bb754beecde0bbe11
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-12-21 21:20:55 +0800

    Update exp7.2.c

--- exp7.2.c
+++ exp7.2.c
@@ -1,22 +1,23 @@
-#include<stdio.h>
-#include<string.h>
-void strcat(char *str1,char *str2)
-{
- int i;
- for(i=0;str+i!=NULL;i++)
- {
-  
- }
-}
-void main()
-{
- char str1[20],str2[20];
- printf("请输入字符串1（长度小于20）：");
- gets(str1);
- printf("请输入字符串2（长度小于20）：");
- gets(str2);
- strcat(&str1[0],str2[0]);
- printf("连接后的字符串为：\n");
- puts(str1);
- printf("\n");
+#include<stdio.h> 
+void mystrcat(char a[],char b[])
+{ 
+ int i=0,j=0; 
+ while(a[i++]!='\0');  
+  i--; 
+ while(b[j]!='\0')
+ {  
+  a[i++]=b[j++]; 
+ }  
+ a[i]='\0';  
+} 
+void main() 
+{ 
+ char a[100],b[100]; 
+ printf("请输入字符串1：");
+ gets(a); 
+ printf("请输入字符串2：");
+ gets(b); 
+ mystrcat(a,b);  
+ printf("1和2连接的结果为：");
+ puts(a);  
 }
