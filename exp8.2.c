f1c84f7c652af6c1ee6224b9c36a04aa854670a5
eastmoe <31646341+eastmoe@users.noreply.github.com>
2017-12-21 21:22:19 +0800

    Update exp8.2.c

--- exp8.2.c
+++ exp8.2.c
@@ -1,23 +1,26 @@
-#include <stdio.h>
-void main()
-{
- FILE *fp1,*fp2;
- if((fp1=fopen("D:\\file1.txt","r"))==NULL)
- {
-  printf("无法打开文件D:\\file1.txt！");
-  exit(0);
+#include <stdio.h> 
+#include <stdlib.h> 
+ void main() 
+ { 
+  char ch;
+  FILE *fp1,*fp2; 
+  if((fp1=fopen("D:\\file1.txt","r"))==NULL) 
+  { 
+   printf("无法打开文件D:\\file1.txt！"); 
+   exit(0); 
+  } 
+  if((fp2=fopen("D:\\file2.txt","a+"))==NULL) 
+  { 
+   printf("无法打开文件D:\\file2.txt！"); 
+   exit(0); 
+  } 
+  ch=fgetc(fp1); 
+  printf("1 "); 
+  for(;ch!=EOF;); 
+  { 
+   ch=fgetc(fp1); 
+   fprintf(fp2,"%c",ch); 
+  } 
+  fclose(fp1); 
+  fclose(fp2); 
  }
- if((fp2=fopen("D:\\file2.txt","a+"))==NULL)
- {
-  printf("无法打开文件D:\\file2.txt！");
-  exit(0);
- }
- char cht;
- for(;fp1!=EOF;);
- {
-  fscanf(fp1,"%c",&cht);
-  sprintf(fp2,"%c",cht);
- }
- fclose(fp1);
- fclose(fp2);
-}
